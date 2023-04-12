6098af1d83812e56c4349d8b43250c01cf094213
IskanderC <90400120+IskanderC@users.noreply.github.com>
2023-04-12 19:25:03 +0300

    Used React hook form validator

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -19,10 +19,16 @@ import { LoadingButton } from '@mui/lab';
 const theme = createTheme();
 
 export default function Login() {
-    const {register, handleSubmit, formState: {isSubmitting}} = useForm()
+    const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
+        mode: 'onTouched'
+    })
 
     async function submitForm(data: FieldValues) {
-        await agent.Account.login(data);
+        try{
+            await agent.Account.login(data);
+        } catch (error){
+            console.log(error);
+        }
     }
 
   return (
@@ -41,17 +47,22 @@ export default function Login() {
               fullWidth
               label="Username"
               autoFocus
-              {...register('username')}
+              {...register('username', {required: 'Username is required'})}
+              error={!!errors.username}
+              helperText={errors?.username?.message}
             />
             <TextField
               margin="normal"
               fullWidth
               label="Password"
               type="password"
-              {...register('password')}
+              {...register('password', {required: 'Password is required'})}
+              error={!!errors.password}
+              helperText={errors?.password?.message}
             />
             <LoadingButton
               loading={isSubmitting}
+              disabled={isValid}
               type="submit"
               fullWidth
               variant="contained"
